fd089f6d06ffa848a778c29b1c4bf95c4cd4a65d
Sergio Barrera <sdbeng@gmail.com>
2015-08-15 15:50:12 -0700

    Add welcome message

--- hello_ruby.rb
+++ hello_ruby.rb
@@ -1,2 +1,8 @@
 puts "Hello guys, welcome to my sticker stand!"
+puts spacing = "************ ***********"
+hello = "This is Alan your friendly neighbor.I'm happy to see you."
+puts time = Time.new
 
+3.times do
+  puts hello
+end
