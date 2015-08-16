01c5161bf476a47b8f423879ff859572e8fd2f99
Sergio Barrera <sdbeng@gmail.com>
2015-08-16 10:01:40 -0700

    Ruby first game v0.3

--- first_game.rb
+++ first_game.rb
@@ -5,3 +5,6 @@ health = 100
 puts "#{name} is my name and my health is #{health}."
 puts dashes
 player1 = "Larry"
+player2 = "curly"
+player3 = "moe"
+puts "Players:\n#{player1}\n#{player2}\n#{player3}"
