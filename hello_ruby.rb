dc50737d573cd689203bfc794d0a10ddb6f4fdbf
Sergio Barrera <sdbeng@gmail.com>
2015-08-15 22:11:07 -0700

    Ruby hello unit1

--- hello_ruby.rb
+++ hello_ruby.rb
@@ -1,4 +1,4 @@
-puts "Hello guys, welcome to my sticker stand!"
+puts "Hello guys, welcome to my Lemonade Stand!"
 puts spacing = "************ ***********"
 hello = "This is Alan your friendly neighbor.I'm happy to see you."
 puts time = Time.new
@@ -6,3 +6,5 @@ puts time = Time.new
 3.times do
   puts hello
 end
+
+puts spacing
