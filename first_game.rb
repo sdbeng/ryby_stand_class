3af5fef1cd6cc99c21bce6ac090b531207c9c2b0
Sergio Barrera <sdbeng@gmail.com>
2015-08-16 12:21:23 -0700

    Ruby first game v0.4

--- first_game.rb
+++ first_game.rb
@@ -7,4 +7,10 @@ puts dashes
 player1 = "Larry"
 player2 = "curly"
 player3 = "moe"
-puts "Players:\n#{player1}\n#{player2}\n#{player3}"
+puts "Players: \n\t#{player1}\n\t#{player2}\n\t#{player3}"
+player1.ljust(30, '.')
+puts dashes
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "The game started on #{formatted_time}"
+puts dashes
