079ee1b6eca43f49eaa5db9d8ab81c42a1c18c51
Sergio Barrera <sdbeng@gmail.com>
2015-08-17 15:16:41 -0700

    Create final game.rb v1.2

--- gamev2.rb
+++ gamev2.rb
@@ -53,16 +53,23 @@ end
 
 #run gamr
 player1 = Player.new("moe")
-
 player2 = Player.new("larry", 60)
-
 player3 = Player.new("curly", 125)
+player4 = Player.new("Alvin")
+player5 = Player.new("Simon", 45)
+player6 = Player.new("Theo", 20)
 
 knuckeheads = Game.new("Knuckeheads")
+chipmunks = Game.new("Chipmunks")
+
 knuckeheads.add_player(player1)
 knuckeheads.add_player(player2)
 knuckeheads.add_player(player3)
-
 knuckeheads.play
+
+chipmunks.add_player(player4)
+chipmunks.add_player(player5)
+chipmunks.add_player(player6)
+chipmunks.play
 
 
