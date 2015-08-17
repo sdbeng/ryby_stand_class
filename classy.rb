16ed054250d0275af9ec1318eaa45226d13e9478
Sergio Barrera <sdbeng@gmail.com>
2015-08-16 21:56:02 -0700

    Ruby classes conceptual v0.4

--- classy.rb
+++ classy.rb
@@ -1,13 +1,20 @@
 #it's all about State and Behaviours!
 
 class Player
+  attr_reader :health
+  attr_accessor :name
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
 
+  def score
+    @health = health + 8
+  end
+
   def to_s
-   "I'm #{@name} and have a health of #{@health}."
+   "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def w00t
@@ -36,5 +43,18 @@ player3.blam
 puts player3
 player3.w00t
 puts player3
+
+puts "attrs section".center(30, '*')
+
+#work w/attributes concepts
+player2 = Player.new("larry",60)
+puts player2.name
+player2.name = "Lawrence"
+puts player2.name
+puts player2.health
+puts player2.score
+puts player2
+
+
 
 
